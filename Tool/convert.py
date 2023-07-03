cde8af54aa75dc4af82c3f8f2efa9eec0958826a
PosetMage <PosetMage@gmail.com>
2023-07-03 12:00:55 +0800

    all

--- Tool/convert.py
+++ Tool/convert.py
@@ -4,12 +4,14 @@ import argparse
 # Create the argument parser
 parser = argparse.ArgumentParser(description='Markdown to HTML converter.')
 parser.add_argument('-i', '--input', type=str, help='Input markdown file')
+parser.add_argument('-t', '--template', type=str, help='template.html')
 
 # Parse the command-line arguments
 args = parser.parse_args()
 
 input_file = args.input
 output_file = args.input.replace('.md', '.html')
+template_file = args.template
 
 with open(input_file, 'r', encoding='utf-8') as md_file:
     lines = md_file.readlines()
@@ -38,7 +40,7 @@ for match in matches:
     tag_dict[tag] = content
 
 # Parse template.html and replace tags with their corresponding content
-with open('template.html', 'r', encoding='utf-8') as template_file:
+with open(template_file, 'r', encoding='utf-8') as template_file:
     template = template_file.read()
 
 pattern = r'\{\{(.*?)\}\}'
@@ -49,6 +51,6 @@ for match in matches:
     if tag in tag_dict:
         template = template.replace('{{' + tag + '}}', tag_dict[tag])
 
-# Save the updated template as output.html
+# Save the updated template as the output file
 with open(output_file, 'w', encoding='utf-8') as html_file:
     html_file.write(template)
